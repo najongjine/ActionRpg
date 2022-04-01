59c9c9b30d23d06ebbe68ddb708362ad8a569332
najongjine <54340913+najongjine@users.noreply.github.com>
2022-04-01 12:39:38 +0900

    .

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -41,10 +41,14 @@ public class PlayerController : MonoBehaviour
                 if (Input.GetAxisRaw("Horizontal") < 0)
                 {
                     theSR.flipX = true;
+                    wpnAnim.SetFloat("dirX", -1f);
+                    wpnAnim.SetFloat("dirY", 0f);
                 }
                 else
                 {
                     theSR.flipX = false;
+                    wpnAnim.SetFloat("dirX", 1f);
+                    wpnAnim.SetFloat("dirY", 0f);
                 }
             }
             else
@@ -52,10 +56,14 @@ public class PlayerController : MonoBehaviour
                 if (Input.GetAxisRaw("Vertical") < 0)
                 {
                     theSR.sprite = playerDirectionSprites[0];
+                    wpnAnim.SetFloat("dirX", 0f);
+                    wpnAnim.SetFloat("dirY", -1f);
                 }
                 else if (Input.GetAxisRaw("Vertical") > 0)
                 {
                     theSR.sprite = playerDirectionSprites[2];
+                    wpnAnim.SetFloat("dirX", 0f);
+                    wpnAnim.SetFloat("dirY", 1f);
                 }
             }
         }
