de776bf16a149cc5370bd99e1d18d10439e7c4be
najongjine <54340913+najongjine@users.noreply.github.com>
2022-04-06 12:58:23 +0900

    jong park

---
+++ Assets/Scripts/EnemyHealthController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthController : MonoBehaviour
+{
+    public float currentHealth;
+
+    public GameObject deathEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth <= 0)
+        {
+            if(deathEffect != null)
+            {
+                Instantiate(deathEffect,transform.position,transform.rotation);
+            }
+            Destroy(gameObject);
+        }
+    }
+
+}
