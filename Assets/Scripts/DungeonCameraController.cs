3b790e6b93a56bb73a12d41b4a6db38b1c061ef1
najongjine <54340913+najongjine@users.noreply.github.com>
2022-04-14 12:45:44 +0900

    jong park

--- Assets/Scripts/DungeonCameraController.cs
+++ Assets/Scripts/DungeonCameraController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class DungeonCameraController : MonoBehaviour
 {
+    public static DungeonCameraController instance;
+    public float moveSpeed;
+    public Vector3 targetPoint;
+
+    private void Awake()
+    {
+        instance = this;    
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetPoint.z=transform.position.z;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position,targetPoint,moveSpeed*Time.deltaTime);
     }
+
 }
--- Assets/Scripts/DungeonRoomActivator.cs
+++ Assets/Scripts/DungeonRoomActivator.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class DungeonRoomActivator : MonoBehaviour
 {
+    public GameObject[] allEnemies;
+    private List<GameObject> clonedEnemies = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject enemy in allEnemies)
+        {
+            enemy.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +20,43 @@ public class DungeonRoomActivator : MonoBehaviour
     {
         
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.ToLower() == "player")
+        {
+            DungeonCameraController.instance.targetPoint=new Vector3(transform.position.x,transform.position.y,DungeonCameraController.instance.targetPoint.z);
+            SpawnEnemies();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            if (PlayerHealthController.instance.currentHealth > 0)
+            {
+                DespawnEnemies();
+            }
+        }
+    }
+
+    private void SpawnEnemies()
+    {
+        foreach (GameObject enemy in allEnemies)
+        {
+            GameObject newEnemy = Instantiate(enemy, enemy.transform.position, enemy.transform.rotation);
+            newEnemy.SetActive(true);
+            clonedEnemies.Add(newEnemy);
+        }
+    }
+
+    private void DespawnEnemies()
+    {
+        foreach (GameObject enemy in clonedEnemies)
+        {
+            Destroy(enemy);
+        }
+
+        clonedEnemies.Clear();
+    }
+
 }
